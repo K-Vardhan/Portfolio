dd5425975cbec3f5c8f76a5df24d9ac969a72d6c
K-Vardhan <kritivardhan020@gmail.com>
2023-04-10 23:49:45 +0530

    Add MUI icons in topbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,17 @@ import Contact from "./components/contact/Contact";
 import "./app.scss"
 
 function App() {
+
   return (
     <div className="app">
-     <Topbar/>
-     <div className="sections">
-      <Intro/>
-      <Portfolio/>
-      <Works/>
-      <Testimonials/>
-      <Contact/>
-     </div>
+      <Topbar/>
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,29 +1,29 @@
 import "./topbar.scss"
-import {Person,Mail} from "@material-ui/icons"
+import { Person, Mail } from "@material-ui/icons"
 
-export default function topbar() {
+export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar ">
       <div className="wrapper">
         <div className="left">
-             <a href="#intro" className="logo">Protfolio.</a>
-             <div className="itemContainer">
-               <Person className="icon"/>
-               <span>+7980139818</span>
-             </div>
+          <a href="#intro" className="logo">Protfolio.</a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>7980139818</span>
+          </div>
 
-             <div className="itemContainer">
-               <Mail className="icon"/>
-               <span>kritivardhan020@gmail.com</span>
-             </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>kritivardhan020@gmail.com</span>
+          </div>
         </div>
         <div className="right">
-              <div className="hamburger">
-                <span className="line1"></span>
-                <span className="line2"></span>
-                <span className="line3"></span>
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
 
-              </div>
+          </div>
         </div>
       </div>
     </div>
