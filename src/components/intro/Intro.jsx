002d52765981fba91e93f26eb50db4e38acb0649
K-Vardhan <kritivardhan020@gmail.com>
2023-04-14 23:44:47 +0530

    Intro Section Add

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ export default function Intro() {
           <img src="assets/man.png" alt="" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Kritivardhan Pandey</h1>
+          <h3>Frontend <span>Developer</span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
